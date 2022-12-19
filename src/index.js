9f1022caa6efd4c7c11b74a743b72a5f4b0934d7
han <ips3471@gmail.com>
2022-12-19 16:18:06 +0900

    unmount styled-components

--- src/index.js
+++ src/index.js
@@ -2,15 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './styles/theme/theme';
-import ListsPresenter from './presenter/lists';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<ThemeProvider theme={theme}>
-			<App />
-		</ThemeProvider>
+		<App />
 	</React.StrictMode>,
 );
