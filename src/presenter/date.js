a347ec97dbad64ae1abb82a530580f5112a1624a
han <ips3471@gmail.com>
2023-01-05 19:54:39 +0900

    highlights d-day upcoming 3 days and also 3 days laten from d-day

--- src/presenter/date.js
+++ src/presenter/date.js
@@ -6,8 +6,11 @@ class DatePresenter {
 	}
 
 	getDate() {
-		if (this.diffDay > 0 && this.diffDay <= 4) {
-			return 'D - ' + this.diffDay;
+		if (this.diffDay >= 0 && this.diffDay <= 3) {
+			return 'D - ' + Math.abs(this.diffDay);
+		}
+		if (this.diffDay > -3 && this.diffDay < 0) {
+			return 'D + ' + Math.abs(this.diffDay);
 		}
 
 		return this.getMonth() + ' ' + this.getDay();
