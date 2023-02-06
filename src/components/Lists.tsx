55ca74469582a0ac94df10af650e652afdbd798c
han <ips3471@gmail.com>
2023-02-06 21:59:38 +0900

    only available for signined user

--- src/components/Lists.tsx
+++ src/components/Lists.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useAuthContext } from '../context/AuthContext';
 import { ListsProps } from '../types/components/components';
 import { ScheduleWithId } from '../types/interfaces/interfaces';
 import List from './list';
 
 function Schedules({ lists }: ListsProps) {
+	const { user } = useAuthContext();
+
 	const sortSchedulesByDate = (a: ScheduleWithId, b: ScheduleWithId) => {
 		return convertDateToCompareWith(a.date) - convertDateToCompareWith(b.date);
 
@@ -20,9 +23,13 @@ function Schedules({ lists }: ListsProps) {
 		<div className='flex-1'>
 			<ul>
 				{lists &&
+					user &&
 					lists.sort(sortSchedulesByDate).map(schedule => {
 						return <List key={schedule.id} list={schedule} />;
 					})}
+				{!user && (
+					<div className='p-5 '> 로그인 후에 리스트 확인이 가능합니다.</div>
+				)}
 			</ul>
 		</div>
 	);
