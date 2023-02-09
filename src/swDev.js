66bb747335a99d3fcf3481fd4f4d575999a6f4a7
han <ips3471@gmail.com>
2023-02-10 01:28:01 +0900

    check sw updated

--- src/swDev.js
+++ src/swDev.js
@@ -1,26 +1,16 @@
 export default function swDev() {
 	const swURL = `${process.env.PUBLIC_URL}/sw.js`;
 
-	navigator.serviceWorker.register(swURL).then(res => {
-		if (res.installing) {
+	navigator.serviceWorker.register(swURL, { scope: '/' }).then(registration => {
+		if (registration.installing) {
 			console.log('Service worker installing');
-		} else if (res.waiting) {
+		} else if (registration.waiting) {
 			console.log('Service worker installed');
-		} else if (res.active) {
+			registration.update().then(() => {
+				console.log('sw check');
+			});
+		} else if (registration.active) {
 			console.log('Service worker active');
 		}
 	});
 }
-
-/* 
-navigator.serviceWorker.register('sw.js').then(function(reg) {
- if(reg.installing) {
-        console.log('Service worker installing');
-    } else if(reg.waiting) {
-        console.log('Service worker installed');
-    } else if(reg.active) {
-        console.log('Service worker active');
-    }
- // Include below mentioned validations
-}
-*/
